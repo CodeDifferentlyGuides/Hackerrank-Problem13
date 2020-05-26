7fee47d61f31dbfb9666015cb16928e76c745939
CodeDifferentlyGuides <65664324+CodeDifferentlyGuides@users.noreply.github.com>
2020-05-26 00:49:06 -0400

    Created SolutionTest.java

---
+++ test/SolutionTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void contructSolutionTest(){
+
+        String expected = "Generic Sports
+                           Each team has n players in Generic Sports
+                           Soccer Class
+                           Each team has 11 players in Soccer Class";
+                           
+        String actual = "*insert main function call*";
+
+        Assert.assertEquals(expected, actual);
+    }
+}
